141eb0218cb8af793a51f45cf9838e24b2c8d321
Victor Andaya <48934786+VictorAndaya@users.noreply.github.com>
2020-07-29 13:36:59 -0500

    controlador de validacion

---
+++ dapper_sisev/Controllers/ValidacionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace dapper_sisev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidacionController : ControllerBase
+    {
+        private string _connection = @"Server=localhost;Port=3306;User=root;Password=123456789;Database=TestVocacionalISC";
+
+        [HttpGet]
+        public IActionResult validation(Models.Usuarios model)
+        {
+            string response = "";
+            try
+            {
+                IEnumerable<Models.Usuarios> lst = null;
+                using (var db = new MySqlConnection(_connection))
+                {
+                    var sql = "SELECT * FROM Usuarios WHERE Usuario = @Usuario AND Contrasena = @Contrasena;";
+                    lst = db.Query<Models.Usuarios>(sql, model);
+                }
+                if (lst.Count()==1)
+                    response = "Se encontro el usuario";
+                else
+                    response = "No se encontro";
+            }
+            catch (Exception e)
+            {
+                response = e.Message;
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ dapper_sisev/Models/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_sisev.Models
+{
+    //public class Usuarios
+    //{
+    //    public string Usuario { get; set; }
+    //    public string Contrasena { get; set; }
+    //}
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public bool Rol { get; set; }
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
